3376ae7b98d8ee3f729ebc805c81e0fb98e3910d
Prashant Joshi <scienceprashant27@gmail.com>
2021-12-28 23:07:41 +0530

    Added test cases

--- src/main/java/com/prashant/employee/model/EmployeeData.java
+++ src/main/java/com/prashant/employee/model/EmployeeData.java
@@ -1,15 +1,19 @@
 package com.prashant.employee.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 public class EmployeeData {
 
     @Getter
+    @Setter
     private String firstname;
 
     @Getter
+    @Setter
     private String lastname;
 
     @Getter
+    @Setter
     private String email;
 }
